2933c508ffaf7ee0decd8a0fe8bf2a62f0d074bd
miyazakis <shota.M.020626.S.K.F@gmail.com>
2020-07-02 21:30:18 +0900

    Update NumericKeypad.ino

--- NumericKeypad/NumericKeypad.ino
+++ NumericKeypad/NumericKeypad.ino
@@ -1,23 +1,23 @@
-#include "keyboard.h"
+#include "Keyboard.h"
 
-const int row = 5;
-const int col = 4;
-
-const int rowPin[row] = {  };
-const int colPin[col] = {  };
-const byte keyTable[row][col] = {
+void setup()
+{
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
   
-};
-
-
-
-
-void setup() {
-  
+  pinMode(5, INPUT_PULLUP);
+  pinMode(6, INPUT_PULLUP);
   
 }
-
-void loop() {
-    
+void loop()
+{
+  if(digitalRead(2) == LOW && digitalRead(5) == LOW)
+  {
+    digitalWrite(17,LOW);
+  }
+  else if(digitalRead(2) == HIGH && digitalRead(5) == HIGH)
+  {
+    digitalWrite(17,HIGH);
+  }
   
 }
