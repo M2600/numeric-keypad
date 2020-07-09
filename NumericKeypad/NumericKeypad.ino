fb05b882b3e5c05db04e27ca732e8a50c8588a64
miyazakis <shota.M.020626.S.K.F@gmail.com>
2020-07-09 15:07:47 +0900

    いろいろ

--- NumericKeypad/NumericKeypad.ino
+++ NumericKeypad/NumericKeypad.ino
@@ -7,17 +7,54 @@ void setup()
   
   pinMode(5, INPUT_PULLUP);
   pinMode(6, INPUT_PULLUP);
+
+  digitalWrite(2,HIGH);
+  digitalWrite(3,HIGH);
+
+  //digitalWrite(5,LOW);
+  //digitalWrite(6,LOW);
+
+  
+  
+  
+  digitalWrite(17,LOW);
+  delay(1000);
+  digitalWrite(17,HIGH);
+  delay(1000);
+  digitalWrite(17,LOW);
+  delay(1000);
+  digitalWrite(17,HIGH);
+  delay(1000);
+  digitalWrite(17,LOW);
+  delay(1000);
+  digitalWrite(17,HIGH);
+  delay(1000);
   
 }
 void loop()
 {
-  if(digitalRead(2) == LOW && digitalRead(5) == LOW)
+  
+  
+  if(digitalRead(5) == LOW && digitalRead(2) == HIGH)
   {
     digitalWrite(17,LOW);
   }
-  else if(digitalRead(2) == HIGH && digitalRead(5) == HIGH)
+  if(digitalRead(5) == LOW && digitalRead(3) == HIGH)
+  {
+    digitalWrite(30,LOW);
+  }
+  else
   {
     digitalWrite(17,HIGH);
+    digitalWrite(30,HIGH);
   }
-  
+  /*if(digitalRead(5) == LOW && digitalRead(3) == LOW)
+  {
+    digitalWrite(30,LOW);
+  }
+  else if(digitalRead(5) == HIGH && digitalRead(3) == HIGH)
+  {
+    digitalWrite(30,HIGH);
+    digitalWrite(17,HIGH);
+  }*/
 }
